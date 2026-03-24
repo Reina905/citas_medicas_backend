f0d8886ac0ea96e1a319148c71028edfa8ad5f90
Kathleen Abigail Argueta Gómez <20245083@esen.edu.sv>
2026-03-24 08:50:12 -0600

    protection para el calendario

--- app/Http/Controllers/HorarioController.php
+++ app/Http/Controllers/HorarioController.php
@@ -6,29 +6,33 @@ use App\Http\Requests\HorarioRequest;
 use App\Http\Resources\HorarioResource;
 use App\Models\Horario;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 
 class HorarioController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     */
     public function index(Request $request)
     {
+        Gate::authorize('viewAny', Horario::class);
         $horarios = Horario::query();
-        $horarios = $horarios 
-        ->when($request->has('dia'), fn ($query) => 
-            $query->where('dia', $request->dia))
-        ->get();
+        $horarios = $horarios
+            ->when($request->has('medico_id'), fn($query) =>
+                $query->where('user_id', $request->input('medico_id')))
+            ->get();
 
         return HorarioResource::collection($horarios);
     }
-  
+
     /**
      * Store a newly created resource in storage.
      */
     public function store(HorarioRequest $request)
     {
+        Gate::authorize('create', Horario::class);
         $data = $request->validated();
-
         $horario = Horario::create($data);
-        $horario->refresh();
 
         return response()->json(HorarioResource::make($horario), 201);
     }
@@ -36,17 +40,26 @@ class HorarioController extends Controller
     /**
      * Display the specified resource.
      */
-     public function show(string $id)
+    public function show(string $id)
     {
-        //
+        $horario = Horario::findOrFail($id);
+        Gate::authorize('view', $horario);
+
+        return response()->json(HorarioResource::make($horario));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(HorarioRequest $request, string $id)
     {
-        //
+        $horario = Horario::findOrFail($id);
+        Gate::authorize('update', $horario);
+
+        $data = $request->validated();
+        $horario->update($data);
+
+        return response()->json(HorarioResource::make($horario));
     }
 
     /**
@@ -54,6 +67,11 @@ class HorarioController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $horario = Horario::findOrFail($id);
+        Gate::authorize('delete', $horario);
+
+        $horario->delete();
+
+        return response()->json(null, 204);
     }
 }
